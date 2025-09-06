d7eaae929614364e66b3839fa4bf00e32b009245
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-09-06 20:46:01 +0100

    parsing : done!

--- cub3D/main.c
+++ cub3D/main.c
@@ -6,7 +6,7 @@
 /*   By: ssbaytri <ssbaytri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 20:26:03 by ssbaytri          #+#    #+#             */
-/*   Updated: 2025/09/06 20:26:56 by ssbaytri         ###   ########.fr       */
+/*   Updated: 2025/09/06 20:45:12 by ssbaytri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,40 @@ void	ss(void)
 {
 	system("leaks cub3D");
 }
+
+void    debug_game(t_game *game)
+{
+    int i;
+
+    printf("========== CONFIG ==========\n");
+    printf("NO: %s\n", game->cfg.no_path);
+    printf("SO: %s\n", game->cfg.so_path);
+    printf("WE: %s\n", game->cfg.we_path);
+    printf("EA: %s\n", game->cfg.ea_path);
+
+    printf("F: [%d, %d, %d]\n",
+        game->cfg.floor_rgb[0],
+        game->cfg.floor_rgb[1],
+        game->cfg.floor_rgb[2]);
+
+    printf("C: [%d, %d, %d]\n",
+        game->cfg.ceil_rgb[0],
+        game->cfg.ceil_rgb[1],
+        game->cfg.ceil_rgb[2]);
+
+    printf("========== MAP ==========\n");
+    printf("Map size: %d x %d\n", game->map.height, game->map.width);
+    printf("Player at: (%d, %d) facing %c\n",
+        game->map.player_pos.x,
+        game->map.player_pos.y,
+        game->map.player_dir);
+
+    for (i = 0; i < game->map.height; i++)
+        printf("[%s]\n", game->map.grid[i]);
+
+    printf("===========================\n");
+}
+
 
 void	free_cfg(t_config *cfg)
 {
@@ -39,6 +73,7 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	printf("Valida Map\n");
+	debug_game(&cub);
 	free2d(cub.map.grid);
 	free_cfg(&cub.cfg);
 	return (0);
